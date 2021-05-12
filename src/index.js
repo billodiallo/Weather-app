908782cf09e79e0444ec9939e3b8b7319313e45a
Billo Diallo <billodiallonet@gmail.com>
2021-05-12 22:27:41 +0300

    Call api with and values , add link

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ import updateDOM, {
 } from './modules/dom';
 import parseJSON from './modules/parsejson';
 
+const apiKey = '06d4a40aeec62ece3ab14b5fc03c4ec0';
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const currentCity = checkCity.value;
+  const fetchWeather = async (city, unit) => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`,
+    );
+    return response.json();
+  };
+
+
