d86452ed1dbc9438484efadaf188fcbc7e68e995
Billo Diallo <billodiallonet@gmail.com>
2021-05-12 20:38:29 +0300

    Get some elements and queryselector for temp, city,date, degrees, fareith

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,3 +1,13 @@
 export const searchForm = document.querySelector('.control');
 export const checkCity = document.getElementById('cityInput');
 export const errorMessage = document.getElementById('warn');
+
+const weatherImg = document.querySelector('.icon');
+const desc = document.querySelector('.weather-status');
+const currTemp = document.querySelector('.main-temp');
+const currCity = document.querySelectorAll('.location>span');
+const moreAbt = document.querySelectorAll('.more-info > p');
+const displayCity = document.getElementById('weather-forecast');
+const currDate = document.querySelector('.date');
+const c = document.getElementById('switchF');
+const d = document.getElementById('degrees');
