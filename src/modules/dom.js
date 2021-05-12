688cb726c24d7c76b81770bf11883c47ddb9c4b8
Billo Diallo <billodiallonet@gmail.com>
2021-05-12 16:56:32 +0300

    Create webpack.config file and dom file

---
+++ src/modules/dom.js
@@ -0,0 +1,3 @@
+export const searchForm = document.querySelector('.control');
+export const checkCity = document.getElementById('cityInput');
+export const errorMessage = document.getElementById('warn');
